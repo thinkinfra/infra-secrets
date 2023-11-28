565b1cc0843a43ebefc94e16d0eada640282d48a
Jeffry <jeffry.milan@earnest.com>
2023-11-28 14:23:51 -0800

    Update precommit to check dotted filenames

---
+++ scripts/precommit-check-dot-filenames.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Initialize a flag to indicate an error
+error_found=0
+
+# Loop through files in the 'secrets' directory
+for file in $(find secrets -type f -name "*.*.*"); do
+    if [[ $file == *"...*" ]]; then
+        # Triple-dotted filenames found
+        echo "Error: Triple-dotted filename not allowed: $file"
+        error_found=1
+    elif [[ $file == *".."* ]]; then
+        # Double-dotted filenames found
+        echo "INFO: Double-dotted filename is used for sub-path: $file"
+    fi
+done
+
+# Exit with an error status if a triple-dotted filename was found
+if [ $error_found -eq 1 ]; then
+    exit 1
+fi
