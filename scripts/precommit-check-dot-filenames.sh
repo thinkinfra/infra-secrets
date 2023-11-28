26b6d2e2c0295c6dd041668be63e3c9d0dbf3229
Jeffry <jeffry.milan@earnest.com>
2023-11-28 14:43:24 -0800

    Test precommit 3 dotted files

--- scripts/precommit-check-dot-filenames.sh
+++ scripts/precommit-check-dot-filenames.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Initialize a flag to indicate an error
 error_found=0
 error_message=""
@@ -13,17 +18,17 @@ for file in $(find secrets -type f -name "*.yaml"); do
 
     if [ $dot_count -gt 2 ]; then
         # Filenames with more than two dots (excluding the .yaml extension)
-        echo "ERROR: Filename with more than two dots not allowed: $file"
+        echo -e "${RED}ERROR: Filename with more than two dots are not allowed: $file${NC}"
         error_found=1
         error_message="Commit failed: Filenames with more than two dots are not allowed in 'secrets' directory."
     elif [ $dot_count -eq 2 ]; then
         # Filenames with exactly two dots
-        echo "INFO: Double-dotted filename detected: $file"
+        echo -e "${GREEN}INFO: Double-dotted filename detected: $file${NC}"
     fi
 done
 
 # Exit with an error status if a problematic filename was found
 if [ $error_found -eq 1 ]; then
-    echo "$error_message"
+    echo -e "${RED}$error_message${NC}"
     exit 1
 fi
